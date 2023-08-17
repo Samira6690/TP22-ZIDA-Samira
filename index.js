5a076cb3fef56609b2da625c72afa47727b6e57c
Samira6690 <zidasamiratou34@gmail.com>
2023-08-17 17:36:35 +0000

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-index.js
+const nav = document.querySelector('nav');
+const Fabars= document.querySelector('fa-bars');
+document.querySelector('.nav-links').onclick = () => {
+    nav.classList.toggle('active');
+}
+Fabars.onclick = () =>{
+    nav.classList.toggle('active');
+}
